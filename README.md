86575e0309800d936b7302b6a1fea0af6b5d8df4
Ashley Winnop <arwinnop@gmail.com>
2018-05-07 19:42:59 -0600

    submission final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Around-the-world-with-GIFs
+
+
+Explore the world with GIFs!
+Select a button to get 10 themed GIFs of that location.
+Add a place to get a new button, then explore your new place by clicking on that button. 
+In order to make the GIFs play, click on the image you want to see.
+To make the GIF stop playing, click on the image again. 
+
+# Have fun exploring!!!
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,8 @@
-var topics = ["australia", "antartica", "china", "usa", "france", "japan", "india", "iceland", "thailand", "russia"];
+var topics = ["australia", "antartica", "canada", "china", "usa", "ethiopia", "france", "japan", "india", "iceland", "thailand", "russia"];
 
 var apiKey = "&api_key=Tz9cVw3eIhSi3UNKdAQ2yYDpixlXKdDc";
+
+var results;
 
 
 function displayInfo() {
@@ -14,26 +16,27 @@ function displayInfo() {
     }).then(function(response){
         console.log(response);
 
-        var results = response.data;
+        results = response.data;
 
         for (var i = 0; results.length; i++) {
-            // get gifs and display
+        
             var gifDiv = $("<div>");
             var gifImage = $("<img>");
 
-            // gifImage.addClass()
-            gifImage.attr("src", results[i].images.fixed_height.url);
+            gifImage.addClass("m-1");
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-state", "still");
+            gifImage.attr("still", results[i].images.fixed_height_still.url);
+            gifImage.attr("animate", results[i].images.fixed_height.url);
             gifDiv.append(gifImage);
 
-            // get rating and display
             var rating = results[i].rating;
             var ratingP = $("<div>").text("Rating: " + rating);
             gifDiv.append(ratingP);
 
-            $("#gif-display").prepend(gifDiv)
+            $("#gif-display").prepend(gifDiv);
         }
     })
-    
 }
 
 function renderButtons() {
@@ -41,8 +44,10 @@ function renderButtons() {
     $("#buttons-view").empty();
     
     for (var i= 0; i < topics.length; i++) {
+
         var a = $("<button>");
-        a.addClass("btn btn-light place-btn");
+
+        a.addClass("btn btn-light place-btn text-center mx-auto col-m-1");
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#buttons-view").append(a);
@@ -53,11 +58,22 @@ $("#add-place").on("click", function(event){
     var userTopic = $("#place-input").val().trim();
     topics.push(userTopic);
     renderButtons();
+    $("#place-input").val("");
 });
+
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log("string");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("still"));
+        $(this).attr("data-state", "still");
+    }
+})
 
 $(document).on("click", ".place-btn", displayInfo);
 
 renderButtons();
-
-
- 
